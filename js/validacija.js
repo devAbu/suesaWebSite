44448933e336ed323d9e59ae14b6004b3cb9b4a8
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-07-06 17:58:23 +0200

    validacija za upit....samo fali da podatke posalje na stranicu koja ce poslat na mail

---
+++ js/validacija.js
@@ -0,0 +1,32 @@
+$('#button').click(function () {
+    var fullName = $('#fullName').val()
+    var email = $('#email').val()
+    var phoneNumber = $('#phoneNumber').val()
+    var message = $('#message').val()
+
+    function validateEmail($email) {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        return emailReg.test($email);
+    }
+    toastr.options.closeButton = true;
+    toastr.options.preventDuplicates = true;
+    toastr.options.progressBar = true;
+    toastr.options.timeOut = 2000;
+    toastr.options.extendedTimeOut = 1000;
+
+    if (fullName == "") {
+        toastr.error('Please enter your name.')
+    } else if (email == "") {
+        toastr.error("Please enter your email address.")
+    } else if (!validateEmail(email)) {
+        toastr.warning("Please enter a valid email address")
+    } else if (phoneNumber == "") {
+        toastr.error("Please enter your phone number.")
+    } else if (phoneNumber == 0 || phoneNumber.length < 6 || phoneNumber.length > 15) {
+        toastr.warning("Please enter a valid number.")
+    } else if (message == "") {
+        toastr.error("Please enter your message.")
+    } else {
+        toastr.success("Hvala.")
+    }
+})
