9e1f81806bb01fbee7610161cc41300f75d041a9
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-07-06 17:35:02 +0200

    js u poseban folder/fajl

---
+++ js/include.js
@@ -0,0 +1,7 @@
+$(window).on("load", function () {
+    $(document).ready(function () {
+        $('#navbarInclude').load('./templates/navbar.html')
+        $('#footerInclude').load("./templates/footer.html");
+
+    })
+})
