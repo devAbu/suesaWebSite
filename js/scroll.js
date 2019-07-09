95cebd3a07346ed91d4fc8a61d65f4263e9f775b
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-07-09 18:02:13 +0200

    eto ga prave slike za 3 kuce koje sam dobio....samo trebaju inf

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+// Menu-toggle button
+
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav > ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+        $('.menu > ul > li > a').addClass('white')
+    } else {
+        $('nav').removeClass('black');
+        $('.menu > ul > li > a').removeClass('white')
+    }
+})
