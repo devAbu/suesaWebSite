fd282c1db7a2b921e7e924057cca19744ca88f18
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-07-17 22:40:15 +0200

    juhu...valjda je to to hahaha

--- emailSend/sendEmail.php
+++ emailSend/sendEmail.php
@@ -7,12 +7,18 @@ $message = $_REQUEST["message"];
 
 if ($_REQUEST['task'] == "message") {
 
-    $admin_email = "abdulrahman.almonajed@gmail.com";
+    $admin_email = "mersad@suessa.com";
+    $admin_email2 = "info@suessa.com";
 
     //send email
     mail($admin_email, "$fullName", $message, "From:" . $email);
     if (mail($admin_email, "$fullName", $message, "From:" . $email)) {
-        echo ('sent');
+        mail($admin_email2, "$fullName", $message, "From:" . $email);
+        if(mail($admin_email2, "$fullName", $message, "From:" . $email)){
+            echo ('sent');
+        }else{
+            echo ('drugi mail ne radi');
+        }
     } else {
         echo ('ne radi');
     }
